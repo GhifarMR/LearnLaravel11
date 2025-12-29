a67e9858529ab15764e734a29383b58482f95ddb
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-29 12:57:18 +0700

    Add Halo and Todo controllers, models, migrations, and views with routing

---
+++ app/Http/Controllers/Todo/TodoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Todo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('toDo.app');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:25'
+        ],[
+            'task.required' => 'isian task wajib diisikan',
+            'task.min' => 'minimal isian untuk task adalah 3 karakter',
+            'task.max' => 'maximal isian untuk task adalah 25 karakter',
+        ]);
+
+        $data = [
+            'task'=>$request->input('task')
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
